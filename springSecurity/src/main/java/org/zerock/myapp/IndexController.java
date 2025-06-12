dc1905ea66800fb2b197dbfa30cc9ac1e89e78cc
taeyoung9601 <168715658+taeyoung9601@users.noreply.github.com>
2025-06-12 20:58:55 +0900

    basic

--- springSecurity/src/main/java/org/zerock/myapp/IndexController.java
+++ springSecurity/src/main/java/org/zerock/myapp/IndexController.java
@@ -1,12 +1,24 @@
 package org.zerock.myapp;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
+
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage(){
+        return "loginPage";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
     }
 }
